b8c956d4636f2639f220ba9680150559ef24c7bb
Toothless101 <divyanibajpai5@gmail.com>
2025-11-21 03:26:22 +0530

    Update server.js

--- aca/server.js
+++ aca/server.js
@@ -3,6 +3,8 @@ const { spawn } = require("child_process");
 const path = require("path");          // 🔥 ADD THIS
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`API server running on port ${PORT}`));
+
 
 
 app.use(express.json());
@@ -958,5 +960,6 @@ app.listen(PORT, () => {
 });
 app.get("/", (req, res) => {
     res.send("Backend running!");});
+
 
 
